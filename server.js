3c6b8277fbc5fa2772bd88b66b6129feef7f1008
Austin Lordi <all321@lehigh.edu>
2020-02-07 18:54:02 -0500

    dynamo getting set up

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const port = process.env.PORT || 5000;
 
 
 var AWS = require("aws-sdk");
+AWS.config.update({region: "us-east-2"});
 var docClient = new AWS.DynamoDB.DocumentClient();
+
+const rand = require('random-item');
 
 app.use(express.static(__dirname + '/client/build'));
 
@@ -13,39 +16,30 @@ app.get("/", (req, res, next) => {
 });
 
 // create a GET route
-app.get('/express_backend', (req, res) => {
-    res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+app.get('/3x3', (req, res) => {
+    var params = {
+        TableName : "abs",
+        FilterExpression: "Difficulty IN (:d1, :d2) and Area = :area", 
+        ExpressionAttributeValues: {
+            ":d1": "Hard",
+            ":d2": "Medium",
+            ":area": "Lower" 
+        }
+    };
+    docClient.scan(params, function(err, data) {
+        if (err) {
+            console.log("Error", err);
+        } else {
+        console.log("Success", data.Items);
+        }
+        var x = rand(data.Items);
+        console.log(x);
+        res.send(x);
+    });
+    
 });
 
 // console.log that your server is up and running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
 
-
-AWS.config.update({endpoint: "https://dynamodb.us-east-2.amazonaws.com"});
-
-
-
-console.log("Querying for movies from 1985.");
-
-var params = {
-    TableName : "Movies",
-    KeyConditionExpression: "#yr = :yyyy",
-    ExpressionAttributeNames:{
-        "#yr": "year"
-    },
-    ExpressionAttributeValues: {
-        ":yyyy": 1985
-    }
-};
-
-docClient.query(params, function(err, data) {
-    if (err) {
-        console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
-    } else {
-        console.log("Query succeeded.");
-        data.Items.forEach(function(item) {
-            console.log(" -", item.year + ": " + item.title);
-        });
-    }
-});
